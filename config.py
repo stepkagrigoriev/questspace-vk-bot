22d87bd56656f0ae728c575d9ad0cd73f85055cf
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2026-04-10 21:37:28 +0500

    ЛАСТОВАЯ

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-VK_TOKEN = os.getenv("VK_API_TOKEN", "").strip()
-API_URL = os.getenv("API_URL", "").strip()
+VK_TOKEN = os.getenv("VK_TOKEN")
+API_URL = os.getenv("API_URL")
