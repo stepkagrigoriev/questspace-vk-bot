fc1d2c6e92e67a2dfe387cb7c490a24e38b4495a
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2026-04-10 21:03:46 +0500

    fixes

--- config.py
+++ config.py
@@ -3,10 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-VK_TOKEN = os.getenv("VK_API_TOKEN")
-VK_GROUP_ID = os.getenv("VK_GROUP_ID")
-MY_VK_ID = int(os.getenv("MY_VK_ID", 0))
-
-API_URL = os.getenv("API_URL")
-API_USERNAME = os.getenv("API_USERNAME")
-API_PASSWORD = os.getenv("API_PASSWORD")
+VK_TOKEN = os.getenv("VK_API_TOKEN", "").strip()
+API_URL = os.getenv("API_URL", "").strip()
